dbcff6f5d36d480475582d039cf266e3d63979a8
Asif Jamal <asif.sanix@gmail.com>
2019-06-25 18:35:05 +0530

    files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function getChildCategories($parents,$mainId=true){
+        if ($mainId==true) {$this->categories[] = $parents = (is_array($parents))?$parents:[$parents]; }
+        $category = DB::table('categories')->select('id')->whereIn('parent',$parents)->get();
+        if ($category->count()) {
+            $this->categories[] = $category->pluck('id');
+            return $this->getChildCategories($category->pluck('id'),false);
+        }
+        $this->categories = collect($this->categories);
+        return $this->categories = $this->categories->collapse();
+    }
 }
